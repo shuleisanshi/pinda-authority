63cdde136f08beab03540243d690e88ec6cdf466
shulei <shuleisanshi@163.com>
2022-10-21 21:04:40 +0900

    新增商品

---
+++ pd-apps/pd-goods/pd-goods-server/src/main/java/com/itheima/pinda/goods/dao/GoodsInfoMapper.java
@@ -0,0 +1,12 @@
+package com.itheima.pinda.goods.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.itheima.pinda.goods.entity.GoodsInfo;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Mapper 接口
+ */
+@Repository
+public interface GoodsInfoMapper extends BaseMapper<GoodsInfo> {
+}
